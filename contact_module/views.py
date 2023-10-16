3c58c6d5d99dd55f080ece5090958f61c0690e57
Sajjad Ahmadizad <96223844+SajjadAhmadizad@users.noreply.github.com>
2023-10-16 15:24:34 +0330

    Update views.py
    if there is no instance of SiteModel

--- contact_module/views.py
+++ contact_module/views.py
@@ -15,15 +15,23 @@ def ContactUsView(request):
     if request.method == 'GET':
         contact_us_form = ContactUsModelForm()
         site = SiteModel.objects.filter(is_active=True).first()
-        return render(request, 'contact_module/contact-us-page.html',
-                      {'form': contact_us_form, 'csrf_token': get_token(request),'address':site.address,'phone':site.phone})
+        if site is not None:
+            return render(request, 'contact_module/contact-us-page.html',
+                          {'form': contact_us_form, 'csrf_token': get_token(request), 'address': site.address,'phone': site.phone})
+        else:
+            return render(request, 'contact_module/contact-us-page.html',
+                          {'form': contact_us_form, 'csrf_token': get_token(request), 'address': 'مازندران بابل','phone': '09931355952'})
 
     if request.method == 'POST':
         contact_us_form = ContactUsModelForm(request.POST)
         if contact_us_form.is_valid():
             contact_us_form.save()
             site = SiteModel.objects.filter(is_active=True).first()
-            send_email('تماس با ما', contact_us_form.cleaned_data.get('email'), {'address': site.address,'phone':site.phone}, 'emails/contact-us-success.html')
+            if site is not None:
+                send_email('تماس با ما', contact_us_form.cleaned_data.get('email'),{'address': site.address, 'phone': site.phone}, 'emails/contact-us-success.html')
+            else:
+                send_email('تماس با ما', contact_us_form.cleaned_data.get('email'),{'address': 'مازندران بابل', 'phone': '09931355952'}, 'emails/contact-us-success.html')
+            
             return JsonResponse({
                 'status': 'success',
                 'message': 'فرم تماس باما با موفقیت ثبت شد'
